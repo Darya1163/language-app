caa26c6101286a83d5e04671f5a101c0afffb065
Darya <you@example.com>
2026-03-07 21:57:14 +0300

    Add Service Worker for PWA

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,35 @@
 const CACHE_NAME = 'lingolearn-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
+// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
   );
+  self.skipWaiting();
 });
 
+// Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        // Return cached or fetch from network
+        return response || fetch(event.request);
       })
   );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
